6b6266858b44677be867aa3cbc1afccd934d0b29
Arianna Butnaru <96354955+arbutnar@users.noreply.github.com>
2023-10-14 21:24:27 +0200

    Update AForm.hpp

--- Module05/ex02/AForm.hpp
+++ Module05/ex02/AForm.hpp
@@ -7,31 +7,30 @@ class Bureaucrat;
 
 class AForm {
 	protected:
-		const std::string name;
-		bool isSigned;
-		const int signGrade;
-		const int execGrade;
-	
+		const	std::string name;
+		bool	isSigned;
+		const	int signGrade;
+		const	int execGrade;
 	public:
 		class GradeTooLowException;
 		class GradeTooHighException;
 		class notSigned;
 		
-		AForm();
-		AForm(std::string name, int signGrade, int execGrade);
-		AForm(const AForm &src);
-        AForm &operator=(const AForm &src);
-		virtual ~AForm();
+		AForm( void );
+		AForm( std::string name, int signGrade, int execGrade );
+		AForm( const AForm &src );
+        	AForm &operator=( const AForm &src );
+		virtual ~AForm( void );
 
-		std::string getName() const;
-		bool getIsSigned() const;
-		void setIsSigned(bool state);
-		int getSignGrade() const;
-		int getExecGrade() const;
-		void beSigned(Bureaucrat *b);
+		std::string getName( void ) const;
+		bool getIsSigned( void ) const;
+		void setIsSigned( bool state );
+		int getSignGrade( void ) const;
+		int getExecGrade( void ) const;
+		void beSigned( Bureaucrat *b );
 
-		virtual void execute(Bureaucrat const &executor) const = 0;
+		virtual void execute( Bureaucrat const &executor ) const = 0;
 
 };
 
-std::ostream &operator<<(std::ostream &os, AForm &f);
+std::ostream &operator<<( std::ostream &os, AForm &f );
