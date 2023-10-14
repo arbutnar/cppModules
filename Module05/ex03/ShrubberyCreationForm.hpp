ede3eb353fbf985bdd37e723f80492e059080ded
Arianna Butnaru <96354955+arbutnar@users.noreply.github.com>
2023-10-14 21:59:23 +0200

    Update ShrubberyCreationForm.hpp

--- Module05/ex03/ShrubberyCreationForm.hpp
+++ Module05/ex03/ShrubberyCreationForm.hpp
@@ -4,20 +4,18 @@
 #include "AForm.hpp"
 
 class ShrubberyCreationForm: public AForm {
-
 	private:
-		std::string target;
-		static std::string asciiTree;
-	
+		std::string		target;
+		static std::string	asciiTree;
 	public:
 		class FileError;
 
-		ShrubberyCreationForm(std::string target);
-		ShrubberyCreationForm(const ShrubberyCreationForm &src);
-        ShrubberyCreationForm &operator=(const ShrubberyCreationForm &src);
-		~ShrubberyCreationForm();
+		ShrubberyCreationForm( std::string target );
+		ShrubberyCreationForm( const ShrubberyCreationForm &src );
+        	ShrubberyCreationForm &operator=( const ShrubberyCreationForm &src );
+		~ShrubberyCreationForm( void );
 
-		std::string getTarget();
-		void writeFile() const;
-		void execute(Bureaucrat const &executor) const;
+		std::string getTarget( void );
+		void writeFile( void ) const;
+		void execute( Bureaucrat const &executor ) const;
 };
