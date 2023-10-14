5ed65a0753079479c73dadc3cb761239f1121da3
Arianna Butnaru <96354955+arbutnar@users.noreply.github.com>
2023-10-14 21:20:18 +0200

    Update PresidentialPardonForm.hpp

--- Module05/ex02/PresidentialPardonForm.hpp
+++ Module05/ex02/PresidentialPardonForm.hpp
@@ -2,17 +2,15 @@
 #include "AForm.hpp"
 
 class PresidentialPardonForm: public AForm {
-
 	private:
 		std::string target;
-
 	public:
-		PresidentialPardonForm(std::string target);
-		PresidentialPardonForm(const PresidentialPardonForm &src);
-        PresidentialPardonForm &operator=(const PresidentialPardonForm &src);
-		~PresidentialPardonForm();
+		PresidentialPardonForm( std::string target );
+		PresidentialPardonForm( const PresidentialPardonForm &src );
+        	PresidentialPardonForm &operator=( const PresidentialPardonForm &src );
+		~PresidentialPardonForm( void );
 
-		std::string getTarget();
-		void Zaphod() const;
-		void execute(Bureaucrat const &executor) const;
+		std::string getTarget( void );
+		void Zaphod( void ) const;
+		void execute( Bureaucrat const &executor ) const;
 };
