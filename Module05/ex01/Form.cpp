788b529f0dab794d43c9952e94f4a9cefef52219
Arianna Butnaru <96354955+arbutnar@users.noreply.github.com>
2023-10-14 22:05:40 +0200

    Update Form.cpp

--- Module05/ex01/Form.cpp
+++ Module05/ex01/Form.cpp
@@ -10,11 +10,11 @@ class Form::GradeTooHighException : public std::exception {
 		virtual const char* what() const throw() { return ("Grade too high"); }
 };
 
-Form::Form()
+Form::Form( void )
 	: name ("Unnamed"), isSigned (false), signGrade(0), execGrade(0) {
 }
 
-Form::Form (std::string n, int gts, int gte)
+Form::Form ( std::string n, int gts, int gte )
 	: name (n), isSigned (false), signGrade(gts), execGrade(gte) {
 	if (this->signGrade < 1 || this->execGrade < 1)
         throw Form::GradeTooHighException();
@@ -22,48 +22,48 @@ Form::Form (std::string n, int gts, int gte)
         throw Form::GradeTooLowException();
 }
 
-Form::Form(const Form &src)
+Form::Form( const Form &src )
 	: name (src.name), isSigned (src.isSigned), signGrade(src.signGrade), execGrade(src.execGrade) {
 	*this = src;
 }
 
-Form &Form::operator=(const Form &src) {
+Form &Form::operator=( const Form &src ) {
 	if (this == &src)
 		return (*this);
 	return (*this);
 }
 
-Form::~Form() {
+Form::~Form( void ) {
 }
 
-std::string Form::getName() const {
+std::string Form::getName( void ) const {
 	return (this->name);
 }
 
-bool Form::getIsSigned() {
+bool Form::getIsSigned( void ) {
 	return (this->isSigned);
 }
 
-void Form::setIsSigned(bool state) {
+void Form::setIsSigned( bool state ) {
 	this->isSigned = state;
 }
 
 
-int Form::getSignGrade() const {
+int Form::getSignGrade( void ) const {
 	return (this->signGrade);
 }
 
-int Form::getExecGrade() const {
+int Form::getExecGrade( void ) const {
 	return (this->execGrade);
 }
 
-void Form::beSigned(Bureaucrat *b) {
+void Form::beSigned( Bureaucrat *b ) {
 	if (b->getGrade() > this->getSignGrade())
 		throw Form::GradeTooLowException();
 	this->isSigned = true;
 }
 
-std::ostream &operator<<(std::ostream &os, Form &f) {
+std::ostream &operator<<( std::ostream &os, Form &f ) {
 	os << "Form name: " << f.getName() << ".\nsigned state: " << f.getIsSigned() << ".\ngrade required to sign: " << f.getSignGrade() << ".\ngrade required to execute: " << f.getExecGrade() << ".\n";
 	return (os);
 }
